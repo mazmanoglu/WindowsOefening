cd22f506471fcc161235360c4d85bbcde98bf7b6
fatih ozer <fatihozer246@hotmail.com>
2022-04-04 15:44:35 +0200

    Testing

---
+++ LeeszaalLib.Tests/HatTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LeeszaalLib;
+
+namespace LeeszaalLib.Tests
+{
+    [TestFixture]
+    public class HatTests
+    {
+        [Test]
+        [TestCase(0, "Pet")]
+        [TestCase(1, "Bolhoed")]
+        [TestCase(2, "Pruik")]
+        [TestCase(3, "Niets")]
+        public void Hat_Hats_Return(int index, string hat)
+        {
+            Assert.AreEqual(Hat.Hats(index), hat );
+        }
+    }
+}
---
+++ LeeszaalLib.Tests/ReadingRoomTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeeszaalLib;
+using NUnit.Framework;
+
+namespace LeeszaalLib.Tests
+{
+    [TestFixture]
+    public class ReadingRoomTests
+    {
+
+        private ReadingRoom readingRoom;
+        private Random random;
+        private Reader reader;
+        [SetUp]
+        public void Setup()
+        {
+            readingRoom = new ReadingRoom();
+            random = new Random();
+            reader = new Reader(random);
+            readingRoom.AddReader(10);
+        }
+
+        [Test]
+        public void ReadingRoom_AddReader_ReturnNewReader()
+        {
+            int before = readingRoom.ReadersList.Count;
+            readingRoom.AddReader(10);
+            int after = readingRoom.ReadersList.Count;
+            Assert.AreEqual(before +10, after);
+        }
+
+        [Test]
+
+        public void ReadingRoom_SelectReader_ReturnIndex()
+        {
+            var result = readingRoom.SelectReader(1);
+            Assert.AreEqual(result.ReferenceNumber, readingRoom.ReadersList[1].ReferenceNumber) ;
+        }
+
+        [Test]
+        public void ReadingRoom_NumberOfReaderByHoofdeksel_Returnxx()
+        {
+            bool isHat = Hat.HatNames.Contains(reader.ReaderHat);
+            Assert.IsTrue(isHat);
+        }
+    }
+}
